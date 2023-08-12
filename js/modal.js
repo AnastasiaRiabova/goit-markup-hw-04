c2d8a68b9effd54e30ccdaf9e6b647ce880facd5
AnastasiaRiabova <riabova321@gmail.com>
2023-08-12 14:04:12 +0200

    cleanup

--- js/modal.js
+++ js/modal.js
@@ -1,29 +1,27 @@
-const refs = {
+const { openModalBtn, backdrop, closeModalBtn, modal } = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     backdrop: document.querySelector('.backdrop'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
-refs.backdrop.addEventListener('click', handleBackdropClick);
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+
+const handleBackdropClick = (event) => {
+    if (event.target === backdrop) {
+        toggleModal();
+    }
+}
+
+const handleKeyDown = (event) => {
+    if (event.code === 'Escape' && !modal.classList.contains('is-hidden')) {
+        toggleModal();
+    }
+}
+
+
+openModalBtn.addEventListener('click', toggleModal);
+closeModalBtn.addEventListener('click', toggleModal);
+backdrop.addEventListener('click', handleBackdropClick);
 window.addEventListener('keydown', handleKeyDown);
-
-function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-}
-
-function handleBackdropClick(event) {
-    if (event.target === refs.backdrop) {
-        toggleModal();
-    }
-}
-
-function handleKeyDown(event) {
-    if (event.code === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
-        toggleModal();
-    }
-}
-
-console.log(refs);
